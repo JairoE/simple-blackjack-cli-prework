478699193a82a04e1378b93f4e4b432b7554b93e
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-15 04:35:15 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,9 +57,10 @@ end
 
 def runner
     welcome
-    initial_round
-    while hit? <=21
-      display_card_total(hit?)
+    card_total = initial_round
+    while card_total <= 21
+      hit?(card_total)
+      display_card_total(card_total)
     end
     end_game
 end
